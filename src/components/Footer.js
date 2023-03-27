c1201ce172a8a307f9ffb734eb9cd3b0f72bbde2
theunis007 <56123683+theunis007@users.noreply.github.com>
2023-03-27 15:33:04 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ import SocialLink from './SocialLink'
 const Footer = () => {
   return (
     <footer className='section footer'>
-      <PageLinks parentClass='footer-links' itemClass='footer-link' />
       <ul className='footer-icons'>
         {socialLinks.map((link) => {
           return <SocialLink key={link.id} {...link} itemClass='footer-icon' />
